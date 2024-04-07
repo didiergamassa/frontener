2ff5f90907bcc1514ba79efbd767d25d14297870
didiergamassa <108032706+didiergamassa@users.noreply.github.com>
2024-04-07 21:53:01 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -9,7 +9,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 # Replace with your Heroku app URL
 
 # Adresse du backend Flask
-BACKEND_URL = 'https://backend.herokuapp.com'
+#BACKEND_URL = 'https://backend.herokuapp.com'
 def main():
    menu = ['Introduction','Environnement_d_un_Projet Smart Building','Visu Audit Energie + Deploiement Iot','Visu Solutions Iot + Plan Comptage','Visu Conso Energies(Elec/Gaz/Eau)',"Bilan Conso Energies(Elec/Gaz/Eau)"," Axes d'amélioration identifiées",'Visu Suivi des KPI Conso(Elec/Gaz/Eau)','Visu objectifs Réduction Conso Energies','Info + contacts utiles sites']
    choice=st.sidebar.selectbox("Menu",menu)
